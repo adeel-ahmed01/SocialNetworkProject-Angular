7f90f62b20fea98795e11709d3da3e12f97640d2
chugar <chiheb.berkane@gmail.com>
2019-12-24 23:01:46 +0100

    imporved css; imporved nevbar; added a header for topics;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,35 @@
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatSnackBarModule,
-  MatToolbarModule
-} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BookComponent} from './components/book/book.component';
-import {LibraryComponent} from './components/library/library.component';
-import {RegisterComponent} from './components/register/register.component';
-import {BookRepository} from './services/book.repository';
-import {BookService} from './services/real/book.service';
-import {TopicsModule} from './topics/topics.module';
-import { LoginComponent } from './components/login/login.component';
-import { SpecialTopicsComponent } from './components/special-topics/special-topics.component';
+import {MaterialModule} from "./material/material.module";
+import {NavigationModule} from "./navigation/navigation.module";
+import {PostModule} from "./post/post.module";
+import {CommentListComponent} from "./post/comment-list/comment-list.component";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LibraryComponent,
-    BookComponent,
-    RegisterComponent,
-    LoginComponent,
-    SpecialTopicsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
     AppRoutingModule,
-    MatCardModule,
-    MatIconModule,
-    MatInputModule,
-    MatSnackBarModule,
-    TopicsModule,
+    MaterialModule,
+    NavigationModule,
+    PostModule
 
   ],
   providers: [
-    {provide: BookRepository, useFactory: (http: HttpClient) => new BookService(http), 'deps': [HttpClient]}
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [CommentListComponent]
 })
 export class AppModule {}
